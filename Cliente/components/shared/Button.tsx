ded9af8cc7461fb982fed0e7947abca39bd5cc95
racso80es <racso80es@gmail.com>
2026-01-16 13:26:59 +0100

    Test: Error inyectado para probar Juez

--- Cliente/components/shared/Button.tsx
+++ Cliente/components/shared/Button.tsx
@@ -11,6 +11,8 @@ export interface ButtonProps extends ShadcnButtonProps {
    * Ejemplo: shared-button-confirm, shared-button-cancel
    */
   "data-testid"?: string;
+  // ERROR INYECTADO PARA PROBAR EL JUEZ
+  invalidProperty: number;
 }
 
 /**
